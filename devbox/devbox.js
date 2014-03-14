e5b528f2ae641ef19573c5b84e5fd43005901807
Scott Bishop <scott.bishop@workday.com>
2014-03-13 20:04:07 -0700

    got bootstrap integrated and working on clicking tabs and populating the pages

--- devbox/devbox.js
+++ devbox/devbox.js
@@ -1,7 +1,14 @@
-//http://stackoverflow.com/questions/13899299/write-text-to-clipboard
+var instance_methods = 
+              ["Set Set B, 19$7778",
+              "Se Set B, 24$257",
+              "Se Set B, 24$691",
+              // "Boolean A EQ Boolean B, 24$1964",
+              // "Boolean A NE Boolean B, 94$2902",
+              // "Date A LT Date B, 24$4123",
+              "DateDate B, 24$4713"]; 
 
-
-var methods = ["Set A PLUS Set B, 19$7778",
+var numeric_methods = 
+              ["Set A PLUS Set B, 19$7778",
               "Set A EQ Set B, 24$257",
               "Set A AC Set B, 24$691",
               // "Boolean A EQ Boolean B, 24$1964",
@@ -9,21 +16,19 @@ var methods = ["Set A PLUS Set B, 19$7778",
               // "Date A LT Date B, 24$4123",
               "Date A LE Date B, 24$4713"];  
 
-function XOMethod (name, instanceid) {
-    this.name = name;
-    this.id = instanceid;
-}
 
-function loadXOMethods() {
+
+function loadXOMethods(method_list) {
   xoMethods = new Array();
-  for (var i=0; i<methods.length; i++) {
-    xoMethods.push(new XOMethod (methods[i].split(",")[0], methods[i].split(", ")[1]));
+  for (var i=0; i<methods2.length; i++) {
+    xoMethods.push(new XOMethod (methods2[i].split(",")[0], methods2[i].split(", ")[1]));
   }
   return xoMethods;
 }
 
-function copyToClipboard(name, id) {
-  window.prompt("Copy '" + name + "'' Instance ID:", id);
+function XOMethod (name, instanceid) {
+    this.name = name;
+    this.id = instanceid;
 }
 
 function copy(str) {
@@ -40,33 +45,33 @@ function copy(str) {
 }
 
 
-function handleCopyClick(iDiv, i, id, name) {
+function handleCopyClick(iDiv, id, name) {
     var innerDiv = document.createElement('button');
     innerDiv.innerHTML = name;
     innerDiv.addEventListener("click", function() {
       copy(id);
     });
-
-    iDiv.appendChild(innerDiv); 
+    iDiv.appendChild(innerDiv);
 }
 
-function loadPage() {
-  var xoMethods = loadXOMethods();
+function loadTab(tab, list_of_methods){
+ var xoMethods = loadXOMethods(list_of_methods);
   for (var i=0; i<xoMethods.length;i++) {
     var id = xoMethods[i].id;
     var name = xoMethods[i].name;
     var iDiv = document.createElement('div');
-    iDiv.id = 'block'+i.toString();
-    iDiv.className = 'block';
 
-    handleCopyClick(iDiv, i, id, name);
+    handleCopyClick(iDiv, id, name);
 
-    
-    // Then append the whole thing onto the body
     document.getElementsByTagName('body')[0].appendChild(iDiv);
   }
 }
 
+function loadPage() {
+  var instance = document.getElementById('instance');
+  loadTab(instance, instance_methods);
+  //instance.addEventListener("click", function(){loadTab(instance, instance_methods)});
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   loadPage();

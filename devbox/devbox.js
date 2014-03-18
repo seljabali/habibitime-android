f9b7276e223c58bfa9932d75fe720a9c020cbcd5
Sami Eljabali <sami.eljabali@workday.com>
2014-03-17 17:39:55 -0700

    Added more functions

--- devbox/devbox.js
+++ devbox/devbox.js
@@ -1,48 +1,53 @@
 var instance_methods = 
-              ["Set A or B if A is empty, 19$16631",
-              "Set A PLUS B, 19$7778",
-              "Set A MINUS B, 19$460",              
+              ["Set A + B, 19$7778",
+              "Set A - B, 19$460",              
+              "Set A or B if A is empty, 19$16631",
               "Set A == B, 24$257",
               "Set A == B or A not present, 24$6193",
               "Set A != B, 24$6375",              
-              "Set A AC B, 24$691",
-              "Set A AC B or B Empty, 24$4479",
-              "Set A NC B, 24$2892",
+              "Set A ac B, 24$691",
+              "Set A ac B or B Empty, 24$4479",
+              "Set A nc B, 24$2892",
               "Count of Set A, 18$3506",
               "Count of Set A == 1, 24$10796",
               "Count of Set A >= 1, 24$8129",
               "Count of Set A > 1, 24$15013"]; 
 
 var numeric_methods = 
-              ["Numeric A < B, 24$11603",
+              ["Numeric A + B, 35$803",
+              "Numeric A * B, 35$1667",
+              "Numeric A * -1, 35$188",
+              "Numeric A < B, 24$11603",
               "Numeric A <= B, 24$1014",
               "Numeric A == B, 24$11624",
               "Numeric A >= B, 24$3836",
               "Numeric A > B, 24$1015",
               "Numeric A < 1, 24$10245",
               "Numeric A == 0, 24$13631",
+              "Numeric A != 0, 94$8596",
               "Numeric A > 0, 24$340",
               "Numeric A == 1, 24$1179",              
               "Numeric A > 1, 24$5181",
-              "Numeric A between B & C, 24$11080"];  
+              "Numeric A between B & C, 24$11080"]; 
 
 var date_methods = 
-              ["Date A < B, 24$4123",
+              ["Date A plus 1 Day, 92$24",
+              "Date A plus N Days parm, 92$222",
+              "Date A minus 1 Day, 92$25",
+              "Date A < B, 24$4123",
               "Date A <= B, 24$4713",
               "Date A == B, 24$2749",
-              "Date A >= Date B, 24$1557",
-              "Date A > Date B, 24$1677",
-              "Date A in Date Range, 24$22",
-              "Date A plus 1 Day, 92$24",
-              "Date A plus N Days parm, 92$222",
-              "Date A minus 1 Day, 92$25"];
+              "Date A >= B, 24$1557",
+              "Date A > B, 24$1677",
+              "Date A in Date Range, 24$22"];
 
 var text_methods = 
-              ["String A == B, 24$381",
-              "String A + B, 30$1544",
+              ["String A + B, 30$1544",
               "String A + ' ' + B, 30$1366",
               "String A substring, 66$11",
-              "Formatted Date, 66$117"];              
+              "Formatted Date, 66$117",
+              "String A == B(case-insensitive), 24$381",
+              "String A == B(case-sensitive), 24$3061"];              
 
 var boolean_methods = 
               ["Boolean A == B, 24$1964",
@@ -58,6 +63,9 @@ var favorites_methods =
               "True, 86$13",
               "False, 86$166",
               "Empty Set, 87$118",
+              "0, 86$26783",
+              "1, 86$24",
+              "Processing Person, 87$77408",
               "System Current Moment, 86$21",
               "Processing Effective Moment, 86$1511",
               "Processing Effective Date, 86$46747",
@@ -117,16 +125,17 @@ function loadTab(tab, list_of_methods){
 
 function loadPage() {
   var instance = document.getElementById('instance');
-  instance.addEventListener("click", function(){loadTab(instance, instance_methods)});
   var numeric = document.getElementById('numeric');
-  numeric.addEventListener("click", function(){loadTab(numeric, numeric_methods)});
   var date = document.getElementById('date');
-  date.addEventListener("click", function(){loadTab(numeric, date_methods)});
   var text = document.getElementById('text');
-  text.addEventListener("click", function(){loadTab(text, text_methods)});
   var bool = document.getElementById('boolean');  
-  bool.addEventListener("click", function(){loadTab(bool, boolean_methods)});
   var favorites = document.getElementById('favorites');
+  
+  instance.addEventListener("click", function(){loadTab(instance, instance_methods)});  
+  numeric.addEventListener("click", function(){loadTab(numeric, numeric_methods)});
+  date.addEventListener("click", function(){loadTab(numeric, date_methods)});
+  text.addEventListener("click", function(){loadTab(text, text_methods)});
+  bool.addEventListener("click", function(){loadTab(bool, boolean_methods)});
   favorites.addEventListener("click", function(){loadTab(favorites, favorites_methods)});
 }
 
